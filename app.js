e3bd4a9b75237b5d03f220f834fa6251084b079b
nini4974 <144515766+nini4974@users.noreply.github.com>
2023-12-19 18:18:27 +0300

    update
    added try and catch error handler

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const form = document.querySelector("form");
-const inputE1 = document.querySelector(".search-input");
 const resultContainer = document.querySelector(".result-container");
+const inputE1 = document.querySelector(".search-input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // inputE1.focus();
+
   let searchedValue = inputE1.value;
   inputE1.value = "";
-  resultContainer.innerHTML = "";
-  inputE1.focus();
   findDrinks(searchedValue);
+
+  resultContainer.innerHTML = "";
 });
 
 async function findDrinks(cocktail) {
-  let res = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
-  );
-  let data = await res.json();
+  try {
+    let res = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+    );
+    let data = await res.json();
 
-  for (i = 0; i < data.drinks.length; i++) {
-    console.log(data.drinks[i].strDrink);
+    for (i = 0; i < data.drinks.length; i++) {
+      // console.log(data.drinks[i].strDrink);
 
-    let drinkDiv = document.createElement("div");
-    drinkDiv.setAttribute("class", "drink-card");
+      let drinkDiv = document.createElement("div");
+      drinkDiv.setAttribute("class", "drink-card");
 
-    drinkDiv.innerHTML = `
+      drinkDiv.innerHTML = `
     <div class="img-container">
-      <img src="${data.drinks[i].strDrinkThumb}"/>
+      <img src="${data.drinks[i].strDrinkThumb}" />
     </div>
     <h3>${data.drinks[i].strDrink}</h3>
     `;
 
-    resultContainer.appendChild(drinkDiv);
+      resultContainer.appendChild(drinkDiv);
+    }
+  } catch (er) {
+    alert(`No cocktails found with that ${cocktail} name!`);
   }
 }
